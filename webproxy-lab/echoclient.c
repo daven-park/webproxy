1e95d98d12d669a531a0dbe67496a52970c2cdd7
ChangHyun Park <buschanny@gmail.com>
2025-05-05 10:36:53 +0900

    refactor : c formatting

--- webproxy-lab/echoclient.c
+++ webproxy-lab/echoclient.c
@@ -1,20 +1,23 @@
 #include "csapp.h"
 
-int main(int argc, char **argv){
-    int clientfd;       // 커널에서 매핑시킨 파일의 인덱스 선언
-    char *host, *port, buf[MAXLINE];// ip, port, buf사이즈 정의
-    rio_t rio;          // roburst io 선언
+int main(int argc, char **argv)
+{
+    int clientfd;                    // 커널에서 매핑시킨 파일의 인덱스 선언
+    char *host, *port, buf[MAXLINE]; // ip, port, buf사이즈 정의
+    rio_t rio;                       // roburst io 선언
 
-    if(argc != 3) {     // 매개변수가 다 안들어오면 오류
+    if (argc != 3)
+    { // 매개변수가 다 안들어오면 오류
         fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
     }
-    host = argv[1];     // 매개변수로 입력한 ip
-    port = argv[2];     // 매개변수로 입력한 포트
-    
-    clientfd = Open_clientfd(host, port);   // wrapper함수로 fd 받기
-    Rio_readinitb(&rio, clientfd);          
+    host = argv[1]; // 매개변수로 입력한 ip
+    port = argv[2]; // 매개변수로 입력한 포트
 
-    while(Fgets(buf, MAXLINE, stdin) != NULL){  
+    clientfd = Open_clientfd(host, port); // wrapper함수로 fd 받기
+    Rio_readinitb(&rio, clientfd);
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL)
+    {
         Rio_writen(clientfd, buf, strlen(buf));
         Rio_readlineb(&rio, buf, MAXLINE);
         Fputs(buf, stdout);
--- webproxy-lab/echoserveri.c
+++ webproxy-lab/echoserveri.c
@@ -9,16 +9,18 @@ int main(int argc, char **argv)
     struct sockaddr_storage clientaddr;
     char client_hostname[MAXLINE], client_port[MAXLINE];
 
-    if(argc != 2){
+    if (argc != 2)
+    {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(0);
     }
 
     listendfd = Open_listenfd(argv[1]);
-    while(1) {
+    while (1)
+    {
         clientlen = sizeof(struct sockaddr_storage);
         connfd = Accept(listendfd, (SA *)&clientaddr, &clientlen);
-        Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+        Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
         printf("Connected to (%s, %s)\n", client_hostname, client_port);
         echo(connfd);
         Close(connfd);
@@ -26,13 +28,15 @@ int main(int argc, char **argv)
     exit(0);
 }
 
-void echo(int connfd){
+void echo(int connfd)
+{
     size_t n;
     char buf[MAXLINE];
     rio_t rio;
 
     Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+    while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
+    {
         printf("server received %d bytes\n", (int)n);
         Rio_writen(connfd, buf, n);
     }
